de5e131f06b638acf42ba758158831809ac0386a
e <2312187195@qq.com>
2021-01-05 16:31:15 +0800

    登录完成-登陆前的地址跳转或首页跳转

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -74,6 +74,8 @@ export default {
         // 2、更新vuex和本地存储
         this.updataUser({ user: res }) // 传参数payload
         this.$mynotify({ message: '登录成功', type: 'success' })
+        const beforePath = this.$route.query.redirectUrl // 获取登陆前的地址，如果登录前有地址就跳转到登录前的地址否则跳首页
+        this.$router.push(beforePath || '/')
       }
     },
     ...mapMutations(['updataUser'])
