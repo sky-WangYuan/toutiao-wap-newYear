936a178b3c6d5a62fb26604faa5d3e259a442e97
e <2312187195@qq.com>
2021-01-05 21:52:17 +0800

    下拉刷新业务-假数据

--- src/views/home/components/article_list.vue
+++ src/views/home/components/article_list.vue
@@ -1,14 +1,21 @@
 <template>
    <!-- 内容区 阅读记忆-->
         <div class="scroll-wrapper">
-            <van-list
-            v-model="loading"
-            :finished="finished"
-            finished-text="没有更多了"
-            @load="onLoad"
-            >
-            <van-cell v-for="item in articleList" :key="item" :title="item" />
-            </van-list>
+            <!-- 下拉刷新 -->
+            <van-pull-refresh
+                v-model="onrefresh"
+                :success-text="refreshText"
+                @refresh="onRefresh"
+                >
+                <van-list
+                v-model="loading"
+                :finished="finished"
+                finished-text="没有更多了"
+                @load="onLoad"
+                >
+                <van-cell v-for="item in articleList" :key="item" :title="item" />
+                </van-list>
+            </van-pull-refresh>
         </div>
 </template>
 
@@ -19,10 +26,13 @@ export default {
     return {
       loading: false,
       finished: false,
-      articleList: []
+      articleList: [],
+      onrefresh: false, // 刷新状态
+      refreshText: '' // 刷新成功提示文字
     }
   },
   methods: {
+    // 上拉加载功能
     onLoad () {
       console.log('加载数据')
       setTimeout(() => { // 模拟ajax经过1s后请求的数据
@@ -33,8 +43,21 @@ export default {
           this.articleList.push(...res)
           this.loading = false
         } else {
-          this.finished = true
+          this.finished = true // 显示加载完成
         }
+      }, 1000)
+    },
+    // 下拉刷新功能
+    onRefresh () {
+    //   console.log('下拉刷新')
+      setTimeout(() => {
+        // 1、数据
+        const res = Array.from(Array(10), (value, index) => index + 1)
+        // 2、数据放在list头部
+        this.articleList.unshift(...res)
+        // 3、关闭状态+提示文字
+        this.onrefresh = false
+        this.refreshText = `刷新了${res.length}条数据`
       }, 1000)
     }
   }
