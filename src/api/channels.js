b7302631fb6fb4e28e95ee67a666ae62dff6efda
e <2312187195@qq.com>
2021-01-13 11:52:54 +0800

    封装删除频道的请求api

--- src/api/channels.js
+++ src/api/channels.js
@@ -6,33 +6,51 @@ const tourist = 'tourist-youke'
 const user = 'login-user'
 
 // 获取我的频道
-// export const Channels = () => {
-//   // axios返回一个promise对象
-//   return request({
-//     url: '/user/channels'
-//   })
-// }
-// export const Channels = () => {
-export function Channels () {
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async function (resolve, reject) {
-    const key = store.state.user.token ? user : tourist
-    const str = localStorage.getItem(key) // 查看缓存中的Key
-    if (str) {
-      resolve({ channelList: JSON.parse(str) })
-      // debugger
-    } else {
-      // 线上获取，保存在本地
-      const data = await request({ url: '/user/channels' })
-      localStorage.setItem(key, JSON.stringify(data.channels))
-      resolve(data)
-    }
+export const Channels = () => {
+  // axios返回一个promise对象
+  return request({
+    url: '/user/channels'
   })
 }
+
+// export const Channels = () => {
+// export function Channels () {
+//   // eslint-disable-next-line no-async-promise-executor
+//   return new Promise(async function (resolve, reject) {
+//     const key = store.state.user.token ? user : tourist
+//     const str = localStorage.getItem(key) // 查看缓存中的Key
+//     if (str) {
+//       resolve({ channelList: JSON.parse(str) })
+//       // debugger
+//     } else {
+//       // 线上获取，保存在本地
+//       const data = await request({ url: '/user/channels' })
+//       localStorage.setItem(key, JSON.stringify(data.channels))
+//       resolve(data)
+//     }
+//   })
+// }
 
 // 获取所有频道
 export const allChannels = () => {
   return request({
     url: '/channels'
+  })
+}
+
+// 删除频道
+export const delChannel = (id) => {
+  return new Promise((resolve, reject) => {
+    const key = store.state.user.token ? user : tourist
+    const data = JSON.parse(localStorage.getItem(key))
+    const index = data.findIndex(item => item.id === id)
+    if (index > -1) {
+      // 找到了
+      data.splice(index, 1)
+      localStorage.setItem(key, JSON.stringify(data))
+      resolve()
+    } else {
+      reject(new Error('找不到对应的频道'))
+    }
   })
 }
