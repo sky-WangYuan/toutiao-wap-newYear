f8b61ea615f7dad31bc386776d5fdf77bcd2eec8
e <2312187195@qq.com>
2021-01-21 17:11:41 +0800

    编辑个人资料布局及动态绑定数据完成

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -1,12 +1,36 @@
 <template>
-  <div class="container">
-    <van-nav-bar left-arrow title="编辑资料" right-text="保存"></van-nav-bar>
+    <div class="container">
+    <van-nav-bar left-arrow @click-left="$router.back()" title="编辑资料" right-text="保存" ></van-nav-bar>
+    <van-cell-group>
+      <van-cell is-link title="头像"  center>
+        <van-image
+          slot="default"
+          width="1.5rem"
+          height="1.5rem"
+          fit="cover"
+          round
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        />
+      </van-cell>
+      <van-cell is-link title="名称" :value="user.name" />
+      <van-cell is-link title="性别" :value="user.gender===0?'男': '女'"/>
+      <van-cell is-link title="生日" :value="user.birthday" />
+    </van-cell-group>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'profile'
+  name: 'profile',
+  data () {
+    return {
+      user: {
+        name: '喜喜',
+        gender: 0,
+        birthday: '2021-1-21'
+      }
+    }
+  }
 }
 </script>
 
