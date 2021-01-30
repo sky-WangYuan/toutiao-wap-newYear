f996643337abd72c4dcf473952b3fc2a505a4b6b
e <2312187195@qq.com>
2021-01-30 19:24:27 +0800

    解决手机物理返回按键问题

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,13 @@ module.exports = {
 
       }
     }
-  }
+  },
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+    }
+  },
+
+  // 修改配置-将打包后index.html引入的文件改为./
+  publicPath: './'
 }
